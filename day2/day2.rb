5af585c9709c667e71f86a126ae2131fa835a4f2
Christopher Nguyen <christoph.nguyen@gmail.com>
2017-12-05 22:16:40 -0800

    day 1 and 2

---
+++ day2/day2.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+test = <<-DOC
+5 1 9 5
+7 5 3
+2 4 6 8
+DOC
+
+def calculate_row(row)
+  row_array = row.split(" ")
+  high = row_array[0].to_i
+  low = row_array[0].to_i
+  row_array.each do |num|
+    curr_num = num.to_i
+    high = curr_num if curr_num > high
+    low = curr_num if curr_num < low
+  end
+  puts "#{high} - #{low} = #{high - low}"
+  high - low
+end
+
+# checksum = 0
+# test.each_line do |line|
+#   checksum += calculate_row line
+# end
+# puts "checksum: #{checksum}"
+
+spreadsheet = File.open('data').read
+checksum = 0
+spreadsheet.each_line do |line|
+  # puts line
+  checksum += calculate_row line
+end
+puts checksum
